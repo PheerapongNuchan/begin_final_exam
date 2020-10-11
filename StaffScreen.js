867df700f51a744e63d59fe2e9e0b3c72d5d0582
PheerapongNuchan <70960387+PheerapongNuchan@users.noreply.github.com>
2020-10-11 08:51:27 +0700

    Add files via upload

---
+++ StaffScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+
+class StaffScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Add friends here!</Text>
+        
+      </View>
+    );
+  }
+}
+
+// ...
+
+export default StaffScreen;
